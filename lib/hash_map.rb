c3c943a393a753f7c1aa8bbaa3e083f4f644dfcc
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-09-15 19:57:50 -0400

    HashMap: Implement hash method

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -12,7 +12,14 @@ class HashMap
     @length = 0
   end
 
-  def hash(key); end
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
+
+    hash_code
+  end
 
   def set(key, value); end
 
